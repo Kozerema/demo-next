7e88805fa76d938a573b5c3ec92f6b17b5634692
Kozerema <kozeremavika@gmail.com>
2024-01-24 22:12:47 +0200

    Typification of Heading component

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,5 +1,5 @@
-const Heading = ({tag, text}) => {
-  const Tag = tag || "h1"
+const Heading = ({ tag, text }: { tag?: keyof JSX.IntrinsicElements; text: string}) => {
+  const Tag = tag || 'h1';
   return <Tag>{text}</Tag>
 }
 
